e2d1d4559712cbd51940967f35cf9230b8fbbd5d
Songlx516 <songlixue516@live.com>
2026-05-21 17:05:01 +0800

    feat: 添加搜索框美化功能

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import { Context } from 'hydrooj';
+import { join } from 'path';
 
 export async function apply(ctx: Context) {
   ctx.i18n.load('zh', {
     "Ant Design Beautify": "Ant Design 风格美化",
+    "Search": "搜索",
+    "Search problems": "Search problems",
   });
+  
+  ctx.template.register('problem_main.html', join(__dirname, 'templates', 'problem_main.html'));
 }
